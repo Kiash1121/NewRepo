9e707287c6ebfedb66c260bc4a5a1fcb1160ef0e
akash.s-at-089625599494 <akash.s@vserv.com>
2021-12-10 12:30:19 +0530

    First commit3

--- app/src/main/java/com/vmax/samplegit/MainActivity.kt
+++ app/src/main/java/com/vmax/samplegit/MainActivity.kt
@@ -2,13 +2,14 @@ package com.vmax.samplegit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        Toast.makeText(this,"Hellow",Toast.LENGTH_LONG).show()
 
 
     }
