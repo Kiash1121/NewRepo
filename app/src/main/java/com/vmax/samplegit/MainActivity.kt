a6264837fb84e4d9acd9cab39bbf09f77ab8ed38
akash.s-at-089625599494 <akash.s@vserv.com>
2021-12-10 12:32:02 +0530

    First commit4

--- app/src/main/java/com/vmax/samplegit/MainActivity.kt
+++ app/src/main/java/com/vmax/samplegit/MainActivity.kt
@@ -9,7 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this,"Hellow",Toast.LENGTH_LONG).show()
+        Toast.makeText(this,"Hellow",Toast.LENGTH_LONG).show();
+        //Changds
 
 
     }
