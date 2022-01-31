8b0b8a9e5f114ac56e6fcb8d9b64ac2810c6be35
akash.s-at-089625599494 <akash.s@vserv.com>
2022-01-31 13:50:13 +0530

    Junit to TestNg

--- app/src/test/java/com/vmax/samplegit/ExampleUnitTest.java
+++ app/src/test/java/com/vmax/samplegit/ExampleUnitTest.java
@@ -1,14 +1,8 @@
 package com.vmax.samplegit;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
-import static org.junit.Assert.assertEquals;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
